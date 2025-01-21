b2b3e3897094b312532310b6240a6685e077f4c5
Even Rouault <even.rouault@spatialys.com>
2025-01-22 00:10:14 +0100

    CI: try to workaround failure on Mac

--- .github/workflows/mac/install.sh
+++ .github/workflows/mac/install.sh
@@ -4,4 +4,7 @@ set -e
 
 ccache -M 200M
 
+# Avoid using SQLite3 from mono.framework
+sudo rm -rf /Library/Frameworks/Mono.framework
+
 CC="clang" CXX="clang++" CFLAGS="-Werror" CXXFLAGS="-Werror" CMAKE_BUILD_TYPE=RelWithDebInfo ./travis/install.sh
